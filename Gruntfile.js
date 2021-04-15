b1dc88bb976f37bef4cbe485b33c77c4b0f47bc6
cortehz <omanchis@gmail.com>
2021-04-15 14:46:05 +0200

    Added a Procfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,46 @@
 "use strict";
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    uglify: {
+      options: {
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build: {
+        src: "public/libs/earth/**/*.js",
+        dest: "build/awiearth.min.js",
+      },
+    },
+    jshint: {
+      files: ["*.js", "public/libs/earth/**/*.js"],
+      options: {
+        // ignores: [""],
+        globals: {
+          Buffer: false,
+          console: false,
+          exports: false,
+          module: false,
+          process: false,
+          require: false,
+          __dirname: false,
+        },
+        globalstrict: true,
+      },
+    },
+  });
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        jshint: {
-            files: ["*.js", "public/libs/earth/**/*.js"],
-            options: {
-                // ignores: [""],
-                globals: {
-                    Buffer: false,
-                    console: false,
-                    exports: false,
-                    module: false,
-                    process: false,
-                    require: false,
-                    __dirname: false
-                },
-                globalstrict: true
-            }
-        }
-    });
+  //   // Load the plugin that provides the "jshint" task.
+  //   grunt.loadNpmTasks("grunt-contrib-jshint");
 
-    // Load the plugin that provides the "jshint" task.
-    grunt.loadNpmTasks("grunt-contrib-jshint");
+  //   // Default task(s).
+  //   grunt.registerTask("default", ["jshint"]);
 
-    // Default task(s).
-    grunt.registerTask("default", ["jshint"]);
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
+  // Default task(s).
+  grunt.registerTask("default", ["uglify"]);
 };
